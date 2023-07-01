39ec337d671811cd1ce697494ae42ced3ab30246
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-07-01 12:01:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-
 import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
+import celebs from "./controllers/celebs.js";
 
 const app = express();
 const port = 9000;
 import bodyParser from "body-parser";
 import cors from 'cors';
-import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-    organization: "",
-    apiKey: "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"
-});
-
-const openai = new OpenAIApi(configuration);
-
-
-app.use(bodyParser.json());
-app.use(cors());
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+app.use(express.json()); 
+app.use(express.static(path.join(__dirname,"static")));
+app.use(bodyParser.json());
+app.use(cors());
 
 // Read the names of all the JavaScript files in the current directory
 let fileNames = fs.readdirSync(path.join(__dirname,"static"));
 
 // Import and register routes for each JavaScript file
 fileNames = fileNames
-  .filter((fileName) => fileName.endsWith(".js") && fileName !== "index.js")
+  .filter((fileName) => fileName.endsWith(".html"))
 
-  console.log(fileNames);
- app.use(express.json()); 
-app.use(express.static(path.join(__dirname,"static")));
+app.get("/:name", (req, res)=>{
+    const name = req.params.name;
+    if(name in fileNames) return res.sendFile(path.join(__dirname,"static",name+".html"));
+    return res.status(404).sendFile(path.join(__dirname,"static","404.html"));
+});
+
+app.post("/:name", (req, res)=>{
+  const name = req.params.name;
+  console.log(name, req.body)
+  if(name in celebs) return celebs[name](req, res)
+  return res.status(404).sendFile(path.join(__dirname,"static","404.html"));
+})
 // Default route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname,"static","index.html"));
