bc9afce5515113b068cfa2316c846945fa85cdc7
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-08-18 09:46:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,7 @@ import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
 import celebs from "./controllers/celebs.js";
-import { Analytics } from '@vercel/analytics/react';
- 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
-}
+
 const app = express();
 const Port = process.env.PORT || 9000;
 import bodyParser from "body-parser";
